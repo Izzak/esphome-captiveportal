eda83529d2dfa5313d4ed61966ffa51b6c150f32
Tomáš Dočkal <tomas-dockal@seznam.cz>
2024-01-26 16:23:26 +0100

    Update captive_portal.cpp

--- esphome/components/captive_portal/captive_portal.cpp
+++ esphome/components/captive_portal/captive_portal.cpp
@@ -35,7 +35,8 @@ void CaptivePortal::handle_wifisave(AsyncWebServerRequest *request) {
   ESP_LOGI(TAG, "  Password=" LOG_SECRET("'%s'"), psk.c_str());
   wifi::global_wifi_component->save_wifi_sta(ssid, psk);
   wifi::global_wifi_component->start_scanning();
-  request->redirect("/?save&mac=" get_mac_address_pretty().c_str());
+  std::string mac_adresa = get_mac_address_pretty().c_str();
+  request->redirect("/?save&mac="+mac_adresa);
 }
 
 void CaptivePortal::setup() {}
