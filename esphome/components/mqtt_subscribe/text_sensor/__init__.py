4654f69654a8e458293d94c043a8cc4c965adff8
Tomáš Dočkal <tomas-dockal@seznam.cz>
2024-01-28 13:08:41 +0100

    Update __init__.py

--- esphome/components/mqtt_subscribe/text_sensor/__init__.py
+++ esphome/components/mqtt_subscribe/text_sensor/__init__.py
@@ -32,5 +32,5 @@ async def to_code(config):
 
     parent = await cg.get_variable(config[CONF_MQTT_PARENT_ID])
     cg.add(var.set_parent(parent))
-    cg.add(var.set_topic(get_mac_address_pretty().c_str()+config[CONF_TOPIC]))
+    cg.add(var.set_topic(config[CONF_TOPIC]))
     cg.add(var.set_qos(config[CONF_QOS]))
